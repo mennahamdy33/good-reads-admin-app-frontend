2099d30fabaf37a76d37855af413266b0a824cc0
eman-hesham97 <eng.eman.eltaweel@gmail.com>
2022-05-13 19:22:38 +0200

    nabar edit

--- src/components/modal/AuthorModal.js
+++ src/components/modal/AuthorModal.js
@@ -118,7 +118,7 @@
                 <div className="row">
                     <div className="col-md-4">
                         <Form.Group controlId="dob">
-                            <Form.Label>Select Date</Form.Label>
+                            <Form.Label>Select Date:</Form.Label>
                             <Form.Control type="date" name="dob" placeholder="Date of Birth" />
                         </Form.Group>
                     </div>
--- src/components/pages/tabs/Home.js
+++ src/components/pages/tabs/Home.js
@@ -3,6 +3,8 @@ import MyNavbar from "../../layout/navbar/navbar";
 import Author from "./Author";
 import Category from "./Category";
 import Books from "./Books";
+import "./Books.css";
+
 
 function Home(props) {
   const [activePage, setActivePage]=useState(0);
@@ -24,9 +26,13 @@ function Home(props) {
       return <Category/>
     }
   return (
-  <div>
+  <div className="homeDiv">
     <MyNavbar onClickBook={BookHandler} onClickCategory={CategoryHandler} onClickAuthor={AuthorHandler} active={props.active} />
     {props.children}
+    {/* <body> */}
+      {/* <img className="bg" src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGlicmFyeXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="Logo"/> */}
+      {/* <img className="bg" src="https://cdn.wallpapersafari.com/94/46/76pNxP.jpg" alt="Logo"/> */}
+    {/* </body> */}
   </div>);
 }
 export default Home;
